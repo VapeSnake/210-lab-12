8c1f051c397b48354f170119066c72c1a1fa1932
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-22 15:32:48 -0800

    Initial commit

---
+++ 210-lab-12.cpp
@@ -0,0 +1,16 @@
+//COMSC-210 | Lab 12 std::array | Noel Mier-Luna
+#include <iostream>
+#include <array> // For std::array
+#include <fstream> // For file I/O
+#include <algorithm> // For sort and reverse
+#include <numeric> // For accumulate
+using namespace std;
+
+const int SIZE = 10; // Size of the array
+
+int main()
+{
+
+    
+    return 0;
+}
