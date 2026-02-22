49e73964dd8ca4a3ccb65da2efa57fe909811955
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-22 15:42:07 -0800

    2nd Commit: Plan for what features to use.

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -8,9 +8,23 @@ using namespace std;
 
 const int SIZE = 10; // Size of the array
 
+//Features we want to use in this lab:
+//  std::array - A container that encapsulates fixed size arrays.
+//  size() - A member function of std::array that returns the number of elements in the array.
+//  File I/O - Reading from and writing to files using fstream.
+//  Algorithms - Using algorithms like sort and reverse from the <algorithm> library.
+//  Accumulate - Using the accumulate function from the <numeric> library to sum elements in an array.
+// .at() - A member function of std::array that provides bounds-checked access to elements.
+// .begin() and .end() - Member functions of std::array that return iterators to the beginning and end of the array, respectively.
+// rbegin() and rend() - Member functions of std::array that return reverse iterators to the beginning and end of the array, respectively.
+// find() - An algorithm from the <algorithm> library that searches for a specific value in a range of elements using an iterator.
+
 int main()
 {
+    array<int, SIZE> randomArray; // Declare an empty array of integers with size 30.
+    cout << "The size of the array is: " << randomArray.size() << endl; // Display the size of the array.
 
     
+
     return 0;
 }
